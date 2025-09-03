bd546a92d91452f6d15888f7ca27e33011dbdeaa
EtherealWeb5167 <josalgado@uamv.edu.ni>
2025-09-02 19:03:44 -0600

    ejercicios de manejo de errores

---
+++ ejercicio3/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Ingrese su nombre: ");
+string nombre = Console.ReadLine();
+
+Console.Write("Ingrese su numero de whatsapp: ");
+string whatsapp = Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(nombre) || string.IsNullOrWhiteSpace(whatsapp))
+{
+    nombre = null;
+}
+try
+{
+    string normalizacion = nombre.ToUpper();
+    Console.WriteLine("Nombre: " + normalizacion);
+    Console.WriteLine("WhatsApp: " + whatsapp);
+}
+catch (NullReferenceException)
+{
+    Console.WriteLine("Un dato no fue proporcionado. Imposible normalizar los datos del cliente.");
+}
