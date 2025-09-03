14ab7b0c399b7913f9107bb3d5c39491209fcbe3
EtherealWeb5167 <josalgado@uamv.edu.ni>
2025-09-02 18:17:25 -0600

    ejercicio de manejo de errores

---
+++ ejercicio1/Program.cs
@@ -0,0 +1,19 @@
+﻿int costo;
+int participantes;
+int costoporparticipante;
+
+Console.Write("Ingrese el costo total ");
+costo = int.Parse(Console.ReadLine());
+
+Console.Write("Ingrese la cantidad de participantes ");
+participantes = int.Parse(Console.ReadLine());
+
+try
+{
+    costoporparticipante = costo / participantes;
+    Console.WriteLine("El costo por participante es: " + costoporparticipante);
+}
+catch (DivideByZeroException)
+{
+    Console.WriteLine("Error: La cantidad de participantes no puede ser cero.");
+}
